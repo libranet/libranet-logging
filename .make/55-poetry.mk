6a64615eb2bf7b19f6444af2e86b24ec177c6bf8
Wouter Vanden Hove <wouter.vanden.hove@gmail.com>
2023-03-09 18:06:25 +0100

    change extension

--- .make/55-poetry.mk
+++ .make/55-poetry.mk
@@ -14,7 +14,6 @@ create-dirs:
 	mkdir -p var/cache ;\
 	mkdir -p var/cache/vscode ;\
 	mkdir -p var/log ;\
-	mkdir -p var/run ;\
 	mkdir -p var/tmp
 
 
@@ -34,7 +33,7 @@ poetry-update:
 
 
 .PHONY: poetry-build ## run poetry build to create the python-package
-poetry-install:
+poetry-build:
 	poetry build
 
 
@@ -45,4 +44,9 @@ poetry-env-info:
 
 .PHONY: poetry-export-requirements  ## generate a requirements.txt-file
 poetry-export-requirements:
-	poetry export --format requirements.txt requirements.txt
+	poetry export --format requirements.txt --output requirements.txt
+
+
+.PHONY: poetry-export-requirements-docs  ## generate a requirements.txt-file for readthedocs
+poetry-export-requirements-docs:
+	poetry export --format requirements.txt --only=docs --withput-hashes --output docs/requirements.txt
