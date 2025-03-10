96747c58333cade28fbc5aeafca9783f6dfcff68
Wouter Vanden Hove <wouter@libranet.eu>
2025-03-10 11:09:18 +0100

    make __version__ dynamic

--- src/libranet_logging/__init__.py
+++ src/libranet_logging/__init__.py
@@ -1,8 +1,9 @@
 """libranet_logging.__init__"""
 
-__version__ = "1.4.dev0"
-__copyright__ = "Copyright 2015 - 2023 Libranet"
-__license__ = "MIT License"
+# __version__ = "1.4.dev0"
+# __copyright__ = "Copyright 2015 - 2023 Libranet"
+# __license__ = "MIT License"
+from libranet_logging._version import __version__, __copyright__, __license__  # noqa
 
 from libranet_logging.logconfig import initialize, output_logging_tree  # noqa
 from libranet_logging.loglevel import create_loglevel, create_loglevel_trace  # noqa
